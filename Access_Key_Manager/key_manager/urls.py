266171ee946eab1a681abcafd6b975b14ec052ac
Joseph Ackah Armoo <josepharmoo415@gmail.com>
2024-07-11 19:14:13 +0000

    feature - new key request

--- Access_Key_Manager/key_manager/urls.py
+++ Access_Key_Manager/key_manager/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('key-request', views.assign_key, name='key-request'),
     path('signup', views.signup, name='account_signup'),
 ]
--- Access_Key_Manager/key_manager/views.py
+++ Access_Key_Manager/key_manager/views.py
@@ -1,17 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
 from .models import Accesskey
+from django.db.models import Q
+from django.contrib import messages
+import datetime
+
 
 # Create your views here.
 
 @login_required(login_url='account_login')
 def index(request):
-    keys = Accesskey.objects.filter()
+    q = request.GET.get('q') if request.method == 'GET' else ''
+    if q:
+        keys = Accesskey.objects.filter(Q(status__icontains=q), user=request.user)
+    else:
+        keys = Accesskey.objects.filter(user=request.user)
+    
     context = {
         'keys': keys,
     }
     return render(request, 'key_manager/index.html', context)
+
+
+def assign_key(request):
+    active_key = Accesskey.objects.filter(user=request.user, status='ACTIVE')
+    if active_key:
+        messages.info(request, 'You already have an active key!')
+        return redirect('index')
+    else:
+        new_key = Accesskey.objects.create(
+            user = request.user,
+            status = 'ACTIVE',
+            date_procured = datetime.datetime.now(),
+            expiry_date = datetime.datetime.now(),
+
+        )
+        new_key.save()
+        messages.success(request, 'You have a new key!')
+        return redirect('index')
+    
+
 
 def signup(request):
     form = SignUpForm()
