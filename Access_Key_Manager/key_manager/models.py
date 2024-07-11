6451a2518e0f87ad9fbf7d445e012200ed217b9f
Joseph Ackah Armoo <josepharmoo415@gmail.com>
2024-07-11 16:46:46 +0000

    AccessKey model created for the database

--- Access_Key_Manager/key_manager/models.py
+++ Access_Key_Manager/key_manager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+import uuid
+from django.contrib.auth.models import User
 # Create your models here.
+class Accesskey(models.Model):
+    status_choices={
+        'ACTIVE':'active',
+       'EXPIRED':'expired',
+       'REVOKED':'revoked'
+    }
+    key=models.UUIDField(default=uuid.uuid4,editable=False,unique=True)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    status=models.CharField(max_length=50,choices=status_choices)
+    date_procured=models.DateTimeField(auto_now_add=True)
+    expiry_date=models.DateField()
+    
+    def __str__(self):
+        return f'{str(self.key)} for {self.user.username}'
--- Access_Key_Manager/key_manager/views.py
+++ Access_Key_Manager/key_manager/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
+from .models import Accesskey
+
 # Create your views here.
 
+@login_required(login_url='account_login')
 def index(request):
-
-    return render(request, 'key_manager/index.html')
+    keys = Accesskey.objects.filter()
+    context = {
+        'keys': keys,
+    }
+    return render(request, 'key_manager/index.html', context)
 
 def signup(request):
     form = SignUpForm()
